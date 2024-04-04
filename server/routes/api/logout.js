af4b2920210cf7e0490201816153aa839e409ded
oussama <oussema.lamine20@gmail.com>
2024-04-05 00:15:38 +0100

    removinginterval in Dash.jsx

--- server/routes/api/logout.js
+++ server/routes/api/logout.js
@@ -4,17 +4,15 @@ const router = express.Router();
 
 // Logout route
 router.get("/logout", (req, res) => {
-  // Expire the session cookie
-  req.session.cookie.expires = new Date(0);
-
   // Destroy the session
   req.session.destroy((err) => {
     if (err) {
       console.error("Error destroying session:", err);
       res.status(500).send("Internal Server Error");
     } else {
-      res.clearCookie("1P_JAR");
-      res.status(200).json({ message: "Logout successful" }); // Clear the session cookie (replace "connect.sid" with your session cookie name if it's different)
+      // Expire the session cookie
+      res.clearCookie("sessionId");
+      res.status(200).json({ message: "Logout successful" });
     }
   });
 });

dbb068223608accf384f03f56dbf66bd0fe20151
oussama <oussema.lamine20@gmail.com>
2024-04-04 23:22:56 +0100

    optimizing the post to checkAuth

--- server/routes/api/checkAuth.js
+++ server/routes/api/checkAuth.js
@@ -4,19 +4,19 @@ const router = express.Router();
 
 // Middleware to check if user is authenticated
 router.post("/checkAuth", (req, res) => {
-  if (req.session || req.session.username) {
-    const loginTime = req.session.loginTime || "Unknown";
-    const expiryTime = req.session.cookie.expires || "Unknown";
-    // Assuming you have a 'user' object in the session when the user is authenticated
-    return res.json({
-      authenticated: true,
-      username: req.session.username,
-      loginTime,
-      expiryTime,
-    });
-  } else {
+  if (!req.session) {
     return res.json({ authenticated: false });
   }
+
+  const { username, loginTime, cookie } = req.session;
+  const expiryTime = cookie.expires || "Unknown";
+
+  return res.json({
+    authenticated: true,
+    username,
+    loginTime: loginTime || "Unknown",
+    expiryTime,
+  });
 });
 
 module.exports = router;
